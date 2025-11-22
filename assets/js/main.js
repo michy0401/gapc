1d758d016755c91bb4a6d999c28efd22d198066e
Michelle Argueta <michelleargueta401@gmail.com>
2025-11-22 01:16:50 -0600

    dashboard, sidebar, navbar, footer, login

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,22 +6,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const sidebar = document.querySelector('.sidebar');
     const mainContent = document.querySelector('.main-content');
     
-    // Crear el elemento Overlay (sombra de fondo) dinámicamente
+
     const overlay = document.createElement('div');
     overlay.className = 'overlay';
     document.body.appendChild(overlay);
 
-    // Función para abrir/cerrar menú
     function toggleMenu() {
         sidebar.classList.toggle('active');
         overlay.classList.toggle('active');
     }
 
-    // Evento Click en el botón hamburguesa
     if(menuBtn) {
         menuBtn.addEventListener('click', toggleMenu);
     }
 
-    // Evento Click en la sombra (para cerrar al tocar afuera)
     overlay.addEventListener('click', toggleMenu);
 });
